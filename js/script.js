6821f7957c50a08b3d7a20b1f8cf1d46a80ef626
francoromeo <francoromeo5012@gmail.com>
2023-12-17 16:50:25 -0300

    portfolio css

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ tabsContainer.addEventListener('click', (e) =>{
         aboutSection.querySelector(target).classList.add('active');
     }
 })
+
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src =
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src
+}
