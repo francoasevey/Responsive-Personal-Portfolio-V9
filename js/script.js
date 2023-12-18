d9ff0a3005b9048734bf3b1f8961ec93917b9ce0
francoromeo <francoromeo5012@gmail.com>
2023-12-18 14:53:00 -0300

    toggler js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () =>{
     hideSection();
     toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
 });
 function hideSection(){
     document.querySelector("section.active").classList.toggle("fade-out");
@@ -9,6 +10,25 @@ function hideSection(){
 function toggleNavbar(){
     document.querySelector(".header").classList.toggle("active");
 }
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active", "fade-out")
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+        },500);
+    }
+})
 
 const tabsContainer = document.querySelector('.about-tabs'),
 aboutSection = document.querySelector('.about-section');
