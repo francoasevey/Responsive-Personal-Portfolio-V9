68761352d35f25a5fde3c5a6e9009d65fadf6e8f
francoromeo <francoromeo5012@gmail.com>
2023-12-19 23:45:55 -0300

    loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+window.addEventListener("load", ()=>{
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout(()=>{
+    document.querySelector(".page-loader").style.display = "none";
+    },600);
+});
+
 const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () =>{
     hideSection();
